a26d5e113738ed9d5c7a9f4ede8900ecd04bf0a0
José Claudio Cardoso <jose.cardoso@mundiallogistics.com.br>
2026-06-07 15:31:15 -0300

    testes falhados

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,14 +4,14 @@ test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Playwrigt/);
 });
 
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole('link', { name: 'Get started__' }).click();
 
   // Expects page to have a heading with the name of Installation. hdshfjdsfsk
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
